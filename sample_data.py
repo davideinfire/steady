dadbfa39c3bd1d88d4269bccd52750d68f40e623
davideinfire <davidefr@proton.me>
2026-07-25 17:47:36 +0300

    Create sample_data.py with predefined lists
    Added sample data lists for people, objects, colors, and animals.

---
+++ sample_data.py
@@ -0,0 +1,30 @@
+PEOPLE = [
+    "Anna",
+    "Ben",
+    "Chris",
+    "Emma",
+    "Lucas"
+]
+
+OBJECTS = [
+    "backpack",
+    "key",
+    "book",
+    "lamp",
+    "coin"
+]
+
+COLORS = [
+    "red",
+    "blue",
+    "green",
+    "yellow",
+    "white"
+]
+
+ANIMALS = [
+    "fox",
+    "owl",
+    "wolf",
+    "rabbit"
+]
